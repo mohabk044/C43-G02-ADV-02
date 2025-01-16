096c00a1e468939df2f7ba2ed14c6f7aa30dd834
Mohab Khaled <mohabk044@gmail.com>
2025-01-16 19:23:17 +0200

    P2-Q1

---
+++ Assignment/BookFunction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+
+    public class BookFunctions
+    {
+        public static string? GetTitle(Book book)
+        {
+            return book.Title;
+        }
+
+        public static string?[] GetAuthors(Book book)
+        {
+            return book.Authors;
+        }
+
+        public static decimal GetPrice(Book book)
+        {
+            return book.Price;
+        }
+    }
+
+}
+
